575647e920be8a1c5ac1187e73b8df54c6e9cdc9
Chayut Meetham <Chayuttatai@gmail.com>
2026-04-06 13:01:42 +0700

    Initial commit

---
+++ course-main.php
@@ -0,0 +1,288 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="stepbanner">
+    <img class="w-100" src="assets/images/step.png" alt="" srcset="">
+</div>
+
+<main class="container-fluid ">
+
+    <div class="course-content">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="card-main mb-3">
+                    <img class="w-100" src="assets/images/course1.png" alt="" srcset="">
+                    <div class="card-body">
+                        <progress class="progressbar-inp" value="70" max="100"> 70% </progress>
+                        <h5 class="card-title">70 % กำลังเรียน</h5>
+                        <div class="admin-course">
+
+                            <table>
+                                <tr>
+                                    <td>เจ้าของหลักสูตร :</td>
+                                    <td>คุณทวี เสริมศิล</td>
+                                </tr>
+                                <tr>
+                                    <td>ผู้อนุมัติหลักสูตร :</td>
+                                    <td>คุณวิรัช กิจเตชะการ</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-main mb-5">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <p class="mb-0">ระยะเวลา</p>
+                            <div class="wrap-course-clild">
+                                <img src="assets/images/clock.png" alt="" srcset="">
+                                <p class="mx-0">2 ชั่วโมง</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <p class="mb-0">จำนวนบทเรียน</p>
+                            <div class="wrap-course-clild">
+                                <img src="assets/images/book.png" alt="" srcset="">
+                                <p class="mx-0">4 บทเรียน</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-main mb-5">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <p class="mb-0">สถานะ Certificate</p>
+                            <div class="wrap-course-clild">
+                                <img src="assets/images/cer1.png" alt="" srcset="">
+                                <p class="mx-0">ยังไม่ผ่านเงื่อนไข</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <p class="mb-0">แบบประเมินหลักสูตร</p>
+                            <div class="wrap-course-clild">
+                                <img src="assets/images/cer2.png" alt="" srcset="">
+                                <p class="mx-0">ทำแบบประเมิน</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-lg-7">
+
+                <div class="period-day">
+                    <p>Period 170 Day (14 / jul / 2022 - 31 Dec / 2026)</p>
+                </div>
+
+                <div class="course-tab">
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">รายละเอียดหลักสูตร</button>
+                            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">รายการหลักสูตร</button>
+                        </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            <br>
+                            <br>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+                        </div>
+
+                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <div class="course-list">
+                                <div class="list-top">
+                                    <div>
+                                        <span>บทที่ 1</span>
+                                        <span>ชื่อบทเรียน</span>
+                                    </div>
+                                    <button type="button" class="btn btn-secondary d-flex"><img width="24px" class="mx-1" src="assets/images/download-doc.png" alt="" srcset="">เอกสารประกอบการเรียน</button>
+                                </div>
+
+                                <div class="list-course">
+
+                                    <div class="group1">
+                                        <img src="assets/images/check-true.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบก่อนเรียน</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <p>คะแนนที่ได้</p>
+                                        <div class="score-list">
+                                            <p>8/15</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="list-course">
+
+                                    <div class="group1">
+                                        <img src="assets/images/check-haft.svg" alt="" srcset="">
+                                        <p>วิดีโอบทที่ 1</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <img style="width: 20px; height: 20px;" src="assets/images/clock.png" alt="" srcset="">
+                                        <p>30 นาที</p>
+                                        <div class="link-btn">
+                                            <button type="button" class="btn btn-link"><a href="index.php">ดูวิดีโอ</a></button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="list-course">
+
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบหลังเรียน</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <div class="link-btn">
+                                            <button type="button" class="btn btn-link"><a href="index.php">ทำข้อสอบ</a></button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="list-second">
+                                    <div>
+                                        <span>บทที่ 2</span>
+                                        <span>ชื่อบทเรียน</span>
+                                    </div>
+                                    <button type="button" class="btn btn-secondary bg-white d-flex">ไม่มีเอกสารประกอบ</button>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบก่อนเรียน</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>วิดีโอบทที่ 2</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img style="width: 20px; height: 20px;" src="assets/images/clock.png" alt="" srcset="">
+                                        <p>30 นาที</p>
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบหลังเรียน</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-second">
+                                    <div>
+                                        <span>บทที่ 3</span>
+                                        <span>ชื่อบทเรียน</span>
+                                    </div>
+                                    <button type="button" class="btn btn-secondary bg-white d-flex">ไม่มีเอกสารประกอบ</button>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบก่อนเรียน</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>วิดีโอบทที่ 4</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img style="width: 20px; height: 20px;" src="assets/images/clock.png" alt="" srcset="">
+                                        <p>30 นาที</p>
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบหลังเรียน</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+
+                                <div class="list-second">
+                                    <div>
+                                        <span>บทที่ 4</span>
+                                        <span>ชื่อบทเรียน</span>
+                                    </div>
+                                    <button type="button" class="btn btn-secondary bg-white d-flex">ไม่มีเอกสารประกอบ</button>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบก่อนเรียน</p>
+                                    </div>
+
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>วิดีโอบทที่ 4</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img style="width: 20px; height: 20px;" src="assets/images/clock.png" alt="" srcset="">
+                                        <p>30 นาที</p>
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                                <div class="list-course">
+                                    <div class="group1">
+                                        <img src="assets/images/check-lean.svg" alt="" srcset="">
+                                        <p>ทำข้อสอบหลังเรียน</p>
+                                    </div>
+                                    <div class="group2">
+                                        <img src="assets/images/keylog.svg" alt="" srcset="">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
+
+<?php include 'includes/footer.php'; ?>
