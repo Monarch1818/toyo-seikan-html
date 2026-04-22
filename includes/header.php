7a9ec365f5726712791cc1dff2de3cb944af80a8
orasa205 <116577149+orasa205@users.noreply.github.com>
2026-04-22 20:23:03 +0700

    Update link

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@7.2.0/css/fontawesome.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
 </head>
 
