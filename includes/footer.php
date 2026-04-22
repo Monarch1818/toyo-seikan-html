746017e8e5069795ea3e148503643ceea8ff4aca
orasa205 <116577149+orasa205@users.noreply.github.com>
2026-04-22 20:22:23 +0700

    update link

--- includes/footer.php
+++ includes/footer.php
@@ -86,6 +86,7 @@
         tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
     });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
 </body>
