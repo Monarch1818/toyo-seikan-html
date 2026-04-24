5b5926ac20b48e5545593212ed61de938adb933f
orasa205 <116577149+orasa205@users.noreply.github.com>
2026-04-24 13:28:09 +0700

    Create Page News

---
+++ News.php
@@ -0,0 +1,132 @@
+<?php include 'includes/header.php'; ?>
+
+<style>
+    .container-news {
+
+        h3 {
+            text-align: center;
+            margin-top: 2%;
+        }
+
+        h4 {
+            margin-left: 5% !important;
+            margin-top: 2%;
+        }
+
+        .cardnews {
+            margin-top: 20px;
+            position: relative;
+            border-radius: 20px;
+            overflow: hidden;
+        }
+
+        .cardnews img {
+            width: 100%;
+            height: 300px;
+            object-fit: cover;
+            transition: 0.3s;
+        }
+
+        .card-body {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            padding: 20px;
+            color: white;
+            background-color: rgba(3, 1, 13, 0.5);
+
+        }
+
+        .card-body h5 {
+            margin-left: 0 !important;
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .card-body p {
+            margin: 5px 0;
+            font-size: 14px;
+        }
+
+        .card- small {
+            font-size: 13px;
+            opacity: 0.8;
+        }
+    }
+</style>
+
+<main class="container-fluid ">
+    <div class="container-news">
+
+        <h4>ข่าวประชาสัมพันธ์</h4>
+        <h3>ข่าวประชาสัมพันธ์</h3>
+        <div class="container">
+            <div class="row ">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+                    <div class="cardnews">
+                        <img src="https://cdn.pixabay.com/photo/2026/03/03/10/12/tylijura-car-10153221_1280.jpg">
+                        <div class="card-body">
+                            <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            <p><i class="bi bi-calendar4"></i> 10 เมษายน 2564</p>
+                            <small>Name What is Lorem Ipsum</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include 'includes/footer.php'; ?>
