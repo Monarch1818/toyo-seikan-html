a5aa6caf6b63ac657a3d977d6c4d05ce12a01453
orasa205 <116577149+orasa205@users.noreply.github.com>
2026-04-24 13:49:34 +0700

    Create Page Usage

---
+++ Usage.php
@@ -0,0 +1,120 @@
+<?php include 'includes/header.php'; ?>
+
+<style>
+    .container-usage {
+
+        h3 {
+            text-align: center;
+            margin-top: 2%;
+        }
+
+        h4 {
+            margin-left: 5%;
+            margin-top: 2%;
+        }
+
+        .card {
+            border-radius: 15px;
+            overflow: hidden;
+            background: #fff;
+            border: 2px solid #000000;
+            transition: 0.3s;
+        }
+
+        .card img {
+            width: 100%;
+            height: 150px;
+            object-fit: cover;
+            display: block;
+        }
+
+        .card-body {
+            padding: 10px;
+            border-top: 2px solid #000;
+        }
+
+        .card-text {
+            margin: 0;
+        }
+    }
+</style>
+
+<main class="container-fluid ">
+    <div class="container-usage">
+
+        <h4>วิธีการใช้งาน</h4>
+        <h3>วิธีการใช้งาน</h3>
+
+        <div class="container">
+            <div class="row g-4 mt-1">
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">วิธีการสมัครสมาชิก</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">ลืมรหัสผ่าน</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">การเข้าสู่ห้องเรียนออนไลน์</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">การสอบและผลการสอบ</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">วิธีการสมัครสมาชิก</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">ลืมรหัสผ่าน</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">การเข้าสู่ห้องเรียนออนไลน์</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2025/08/15/03/21/swallowtail-butterfly-9776068_1280.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <p class="card-text">การสอบและผลการสอบ</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php include 'includes/footer.php'; ?>
